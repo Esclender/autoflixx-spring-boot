aa534122852d2ba853eabc5abb1a0277a9e64911
Esclender <elesclenderlugo@gmail.com>
2024-09-21 22:07:02 -0500

    base project

---
+++ src/main/java/com/autoflixx/AutoflixxApplication.java
@@ -0,0 +1,13 @@
+package com.autoflixx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflixxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflixxApplication.class, args);
+	}
+
+}
