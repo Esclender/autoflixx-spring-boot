cab32def9bd6106897174c8e3964d560738315e8
Esclender <elesclenderlugo@gmail.com>
2024-10-14 05:45:16 -0500

    refactor: movie entradas by id

--- src/main/java/com/autoflixx/controllers/EntradasController.java
+++ src/main/java/com/autoflixx/controllers/EntradasController.java
@@ -1,15 +1,28 @@
 package com.autoflixx.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.autoflixx.models.MovieModel;
+import com.autoflixx.services.IMovieService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-@RequestMapping("/entradas")
+@RequestMapping("/movie")
 public class EntradasController {
 
-  @GetMapping("")
-  public String getEntradasPreview() {
+  @Autowired
+  private IMovieService service;
+
+  @GetMapping("/view/{id}/entradas")
+  public String getEntradasPreview(Model model, @PathVariable("id") int idMovie) {
+    MovieModel movie = service.getMovieById(idMovie);
+    model.addAttribute("movie", movie);
+
     return "steps/entradas/index";
   }
 
